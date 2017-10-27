d20f4743137eab99779c36e1b58dbbbde1042073
Korigan Payne <koriganp@gmail.com>
2017-10-27 12:48:22 -0600

    Framed uses cases

--- epic/use-cases.php
+++ epic/use-cases.php
@@ -1,2 +1,23 @@
 <!DOCTYPE html>
-<html></html>
+<html lang="en-US">
+	<head>
+		<meta charset="utf-8"/>
+		<meta name="description" content="use cases for capstone"/>
+		<link href="styles/persona.css" rel="stylesheet" type="text/css"/>
+		<link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet"/>
+		<title>Use Cases</title>
+	</head>
+	<body>
+		<header>
+			<nav class="topnav">
+				<a href="george.php">Persona 1</a>
+				<a href="venus.php">Persona 2</a>
+				<a href="use-cases.php">Use Cases</a>
+				<a href="conceptual-model.php">Conceptual Model</a>
+			</nav>
+		</header>
+		<main>
+
+		</main>
+	</body>
+</html>
