1ca23894913e322ad48b3fd1dfe7969e116dd3cd
Korigan Payne <koriganp@gmail.com>
2017-10-27 10:49:10 -0600

    added info about george

--- epic/george.php
+++ epic/george.php
@@ -12,7 +12,8 @@
 			<h1>Persona</h1>
 			<img id="george" src="images/george.jpg" alt="Image of George Smith"/>
 			<h2 id="about">George Smith</h2>
-			<p></p>
+			<p>George is 28 single and focused on his career as a marketing director. He has a long commute to his office downtown because he got a condo near the edge of town were the price of homes was cheaper. He likes to go out on the weekends and spends his free time working out, reading, watching movies, and surfing the web.</p>
+			<p>He noticed a pothole in the road on his way to work one day and wanted an easy and effective way to report his finding to actually get the repair done in a reasonable amount of time.</p>
 			<h2>User Tech</h2>
 			<p class="story">George has a newer HP laptop and a Samsung Galaxy S7.</p>
 			<h2>Frustrations</h2>
