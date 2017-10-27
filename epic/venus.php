e57cbf25fee78f03883696385f571bf18fd480f3
Korigan Payne <koriganp@gmail.com>
2017-10-27 12:03:32 -0600

    Centered venus

--- epic/venus.php
+++ epic/venus.php
@@ -9,24 +9,28 @@
 	</head>
 	<body>
 		<h1>Purpose, Audience and Goal</h1>
-		<ul>
-			<li><strong>Purpose:</strong>This project aims to serve as a medium where Albuquerque residents can report infrastructure incidents
-				 to city officials and utility workers so that they can resolve them.</li>
-			<li><strong>Audience:</strong>The audience of this project will be utility workers, city officials and Albuquerque residents.</li>
-			<li><strong>Goal:</strong>This project will create a platform for Albuquerque residents to submit reports of incidents around Albuquerque to city officials and utility workers.</strong></li>
-		</ul>
+		<div class="centerList">
+			<ul>
+				<li><strong>Purpose:</strong>This project aims to serve as a medium where Albuquerque residents can report infrastructure incidents
+					 to city officials and utility workers so that they can resolve them.</li>
+				<li><strong>Audience:</strong>The audience of this project will be utility workers, city officials and Albuquerque residents.</li>
+				<li><strong>Goal:</strong>This project will create a platform for Albuquerque residents to submit reports of incidents around Albuquerque to city officials and utility workers.</strong></li>
+			</ul>
+		</div>
 		<h1>Persona</h1>
-		<img src="images/issa-rae-edit.jpg" width="400" height="400" alt="Issa Rae">
-		<ul>
-			<li><strong>Name:</strong> Venus Bradley</li>
-			<li><strong>Age:</strong> 37</li>
-			<li><strong>Quote:</strong> "I need to get this incident report resolved"</li>
-			<li><strong>Profession: City of Albuquerque Infrastructure Manager who is responsible for and manages incoming 311 incident reports by local residents so that she can forwarded to respective departments that handle specific incidents.</strong> </li>
-			<li><strong>Technology: Dell Desktop, iPad tablet, Dell Laptop, LG</strong> </li>
-			<li><strong>Attitudes and Behaviors: She is detail-oriented and attentive while off/on the job. </strong></li>
-			<li><strong>Frustrations and Needs:</strong>   </li>
-			<li><strong>Goals:</strong>  </li>
-		</ul>
+		<img class="persona" src="images/issa-rae-edit.jpg" width="400" height="400" alt="Issa Rae">
+			<div class="centerList">
+				<ul>
+				<li><strong>Name:</strong> Venus Bradley</li>
+				<li><strong>Age:</strong> 37</li>
+				<li><strong>Quote:</strong> "I need to get this incident report resolved"</li>
+				<li><strong>Profession: City of Albuquerque Infrastructure Manager who is responsible for and manages incoming 311 incident reports by local residents so that she can forwarded to respective departments that handle specific incidents.</strong> </li>
+				<li><strong>Technology: Dell Desktop, iPad tablet, Dell Laptop, LG</strong> </li>
+				<li><strong>Attitudes and Behaviors: She is detail-oriented and attentive while off/on the job. </strong></li>
+				<li><strong>Frustrations and Needs:</strong>   </li>
+				<li><strong>Goals:</strong>  </li>
+				</ul>
+			</div>
 		<h2>User Story</h2>
 		<p><strong>User Story:</strong></p>
 	</body>
