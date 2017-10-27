291ebcc9c3ce24042a16e873da2644ce0204e688
Kevin Atkins <kaitoxas@gmail.com>
2017-10-27 16:11:41 -0600

    finished 'Venus' persona

--- epic/venus.php
+++ epic/venus.php
@@ -2,27 +2,31 @@
 <html>
 	<head>
 		<meta charset="utf-8"/>
-		<meta name="description" content="first persona for capstone"/>
+		<meta name="description" content="second persona for capstone"/>
 		<link href="styles/persona.css" rel="stylesheet" type="text/css"/>
 		<link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet"/>
-		<title>Maintenance User Persona</title>
+		<meta charset="utf-8"/>
+		<title>Infrastructure Manager User Persona</title>
 	</head>
 	<body>
-		<h1>Persona</h1>
-		<img class="persona" src="images/issa-rae-edit.jpg" width="400" height="400" alt="Issa Rae">
-			<div class="centerList">
-				<ul>
-				<li><strong>Name:</strong> Venus Bradley</li>
-				<li><strong>Age:</strong> 37</li>
-				<li><strong>Quote:</strong> "I need to get this incident report resolved"</li>
-				<li><strong>Profession: City of Albuquerque Infrastructure Manager who is responsible for and manages incoming 311 incident reports by local residents so that she can forwarded to respective departments that handle specific incidents.</strong> </li>
-				<li><strong>Technology: Dell Desktop, iPad tablet, Dell Laptop, LG</strong> </li>
-				<li><strong>Attitudes and Behaviors: She is detail-oriented and attentive while off/on the job. </strong></li>
-				<li><strong>Frustrations and Needs:</strong>   </li>
-				<li><strong>Goals:</strong>  </li>
-				</ul>
-			</div>
-		<h2>User Story</h2>
-		<p><strong>User Story:</strong></p>
+		<h1 class = "header">Persona 2</h1>
+		<img src = "images/issa-rae-edit.jpg" width="400" height="400" alt = "Issa Rae">
+		<h2 class = "about">Venus Bradley</h2>
+		<p>Venus, 37, is the Albuquerque Infrastructure Manager who is responsible for and manages all incoming 311
+			incident reports by local residents so that she can forward incidents to respective departments that
+			resolve and handle specific incidents by their categories. She is detail-oriented and attentive while off/on
+			the job.</p>
+		<h2>User Tech</h2>
+		<p class = "story"><strong>Technology:</strong> Dell Desktop, iPad tablet, Dell Laptop, LG</p>
+		<h2>Frustrations</h2>
+		<div class = "centerList"></div>
+		<ul>
+			<li>Unsorted Incident Reports are overwhelming</li>
+			<li>Wishes medium/application had a 'gauge' feature to determine an incident's urgency.</li>
+			<li>Would like an graphical chart that shows percentages of what categories are least/most reported.</li>
+		</ul>
+		<h3>User Story</h3>
+		<p><strong>User Story:</strong> Venus believes that the current medium/web application could be improved
+			so that she can easily manage incident reports.</p>
 	</body>
 </html>
