267f030d761c4292300f41cbba3ebe4696633cb4
Korigan Payne <koriganp@gmail.com>
2017-10-27 11:35:08 -0600

    added info more about george

--- epic/george.php
+++ epic/george.php
@@ -12,14 +12,15 @@
 			<h1>Persona</h1>
 			<img id="george" src="images/george.jpg" alt="Image of George Smith"/>
 			<h2 id="about">George Smith</h2>
-			<p>George is 28 single and focused on his career as a marketing director. He has a long commute to his office downtown because he got a condo near the edge of town were the price of homes was cheaper. He likes to go out on the weekends and spends his free time working out, reading, watching movies, and surfing the web.</p>
+			<p>George is 28 single and focused on his career as a marketing director. He has a long commute to his office downtown because he got a condo near the edge of town where the price of homes is cheaper. He likes to go out on the weekends and spends his free time working out, reading, watching movies, and surfing the web.</p>
+			<p>Mr. Smith is very tech savvy and has been using the internet for most of his life. He likes to do all of his shopping online and uses streaming services for all his entertainment. He mostly uses his phone to access the web, but also uses his laptop, desktop, or iPad, depending on where he is at and what is handy.</p>
 			<p>He noticed a pothole in the road on his way to work one day and wanted an easy and effective way to report his finding to actually get the repair done in a reasonable amount of time.</p>
 			<h2>User Tech</h2>
-			<p class="story">George has a newer HP laptop and a Samsung Galaxy S7.</p>
+			<p class="story">George has a MacBook Pro, a iPad Air, and an iPhone 7. His desktop is an HP running Windows 10</p>
 			<h2>Frustrations</h2>
 			<div class="centerList">
 				<ul>
-					<li>Wasting time</li>
+					<li>Wasting time on hold</li>
 					<li>Lack of imagination</li>
 					<li>Inability to communicate</li>
 				</ul>
