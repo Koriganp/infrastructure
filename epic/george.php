a5a868e32e62f7d4f3734f4c09bd8edc19d751d9
Korigan Payne <koriganp@gmail.com>
2017-10-27 10:36:35 -0600

    started persona of reporting user

---
+++ epic/george.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en-US">
+	<head>
+		<meta charset="utf-8"/>
+		<meta name="description" content="second persona for capstone"/>
+		<link href="styles/george.css" rel="stylesheet" type="text/css"/>
+		<link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet"/>
+		<title>Reporting User Persona</title>
+	</head>
+	<body>
+		<main>
+			<h1>Persona</h1>
+			<img id="george" src="images/george.jpg" alt="Image of George Smith"/>
+			<h2 id="about">George Smith</h2>
+			<p></p>
+			<h2>User Tech</h2>
+			<p class="story">George has a newer HP laptop and a Samsung Galaxy S7.</p>
+			<h2>Frustrations</h2>
+			<div class="centerList">
+				<ul>
+					<li>Wasting time</li>
+					<li>Lack of imagination</li>
+					<li>Inability to communicate</li>
+				</ul>
+				<h2>User Story</h2>
+				<p class="story"></p>
+			</div>
+		</main>
+	</body>
+</html>
