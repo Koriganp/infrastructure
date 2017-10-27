d10acd63b293d7032781478ea612c09598498508
Tanisha Purnell <tanishad93@gmail.com>
2017-10-27 10:31:03 -0700

    updated venus.php persona created images folder for issa-rae-edit.jpg

---
+++ epic/venus.php
@@ -0,0 +1,35 @@
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>infrastructure</title>
+	</head>
+	<body>
+		<h1>Purpose, Audience and Goal</h1>
+		<ul>
+			<li><strong>Purpose:</strong> </li>
+			<br>
+			<li><strong>Audience:</strong> </li>
+			<br>
+			<li><strong>Goal:</strong> </strong></li>
+		</ul>
+		<h3>Persona</h3>
+		<img src ="images/issa-rae-edit.jpg" width="400" height="400" alt="Issa Rae">
+		<ul>
+			<li><strong>Name:</strong> Venus Bradley</li>
+			<li><strong>Age:</strong> 37</li>
+			<br>
+			<li><strong>Profession:</strong> </li>
+			<br>
+			<li><strong>Technology:</strong> </li>
+			<br>
+			<li><strong>Attitudes and Behaviors:</strong></li>
+			<br>
+			<li><strong>Frustrations and Needs:</strong>   </li>
+			<br>
+			<li><strong>Goals:</strong>  </li>
+			<br>
+		</ul>
+		<h3>User Story</h3>
+		<p><strong>User Story:</strong> </p>
+	</body>
+</html>
