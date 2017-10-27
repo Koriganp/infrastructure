fbe22023d62355453f641618ada3d56fc6a65c51
Korigan Payne <koriganp@gmail.com>
2017-10-27 11:57:11 -0600

    removed br tags

--- epic/venus.php
+++ epic/venus.php
@@ -2,38 +2,32 @@
 <html>
 	<head>
 		<meta charset="utf-8"/>
-		<title>infrastructure</title>
+		<meta name="description" content="first persona for capstone"/>
+		<link href="styles/persona.css" rel="stylesheet" type="text/css"/>
+		<link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet"/>
+		<title>Maintenance User Persona</title>
 	</head>
 	<body>
 		<h1>Purpose, Audience and Goal</h1>
 		<ul>
 			<li><strong>Purpose:</strong>This project aims to serve as a medium where Albuquerque residents can report infrastructure incidents
 				 to city officials and utility workers so that they can resolve them.</li>
-			<br>
 			<li><strong>Audience:</strong>The audience of this project will be utility workers, city officials and Albuquerque residents.</li>
-			<br>
 			<li><strong>Goal:</strong>This project will create a platform for Albuquerque residents to submit reports of incidents around Albuquerque to city officials and utility workers.</strong></li>
 		</ul>
 		<h1>Persona</h1>
-		<img src ="images/issa-rae-edit.jpg" width="400" height="400" alt="Issa Rae">
+		<img src="images/issa-rae-edit.jpg" width="400" height="400" alt="Issa Rae">
 		<ul>
 			<li><strong>Name:</strong> Venus Bradley</li>
 			<li><strong>Age:</strong> 37</li>
-			<li><strong>Qoute:</strong> "I need to get this incident report resolved"</li>
-			<br>
-			<li><strong>Profession: City of Albuquerque Infrastructure Manager who is responsible for and manages incoming 311
-							incident reports by local residents so that she can forwarded to respective departments that handle specific incidents.</strong> </li>
-			<br>
+			<li><strong>Quote:</strong> "I need to get this incident report resolved"</li>
+			<li><strong>Profession: City of Albuquerque Infrastructure Manager who is responsible for and manages incoming 311 incident reports by local residents so that she can forwarded to respective departments that handle specific incidents.</strong> </li>
 			<li><strong>Technology: Dell Desktop, iPad tablet, Dell Laptop, LG</strong> </li>
-			<br>
 			<li><strong>Attitudes and Behaviors: She is detail-oriented and attentive while off/on the job. </strong></li>
-			<br>
 			<li><strong>Frustrations and Needs:</strong>   </li>
-			<br>
 			<li><strong>Goals:</strong>  </li>
-			<br>
 		</ul>
-		<h3>User Story</h3>
-		<p><strong>User Story:</strong> </p>
+		<h2>User Story</h2>
+		<p><strong>User Story:</strong></p>
 	</body>
 </html>
