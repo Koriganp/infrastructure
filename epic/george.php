49565d0e4e2745ae698ffa7a8304ad0d98593971
Korigan Payne <koriganp@gmail.com>
2017-10-27 11:40:56 -0600

    made stylesheet work for both personas

--- epic/george.php
+++ epic/george.php
@@ -3,15 +3,15 @@
 	<head>
 		<meta charset="utf-8"/>
 		<meta name="description" content="second persona for capstone"/>
-		<link href="styles/george.css" rel="stylesheet" type="text/css"/>
+		<link href="styles/persona.css" rel="stylesheet" type="text/css"/>
 		<link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet"/>
 		<title>Reporting User Persona</title>
 	</head>
 	<body>
 		<main>
 			<h1>Persona</h1>
-			<img id="george" src="images/george.jpg" alt="Image of George Smith"/>
-			<h2 id="about">George Smith</h2>
+			<img class="persona" src="images/george.jpg" alt="Image of George Smith"/>
+			<h2 class="about">George Smith</h2>
 			<p>George is 28 single and focused on his career as a marketing director. He has a long commute to his office downtown because he got a condo near the edge of town where the price of homes is cheaper. He likes to go out on the weekends and spends his free time working out, reading, watching movies, and surfing the web.</p>
 			<p>Mr. Smith is very tech savvy and has been using the internet for most of his life. He likes to do all of his shopping online and uses streaming services for all his entertainment. He mostly uses his phone to access the web, but also uses his laptop, desktop, or iPad, depending on where he is at and what is handy.</p>
 			<p>He noticed a pothole in the road on his way to work one day and wanted an easy and effective way to report his finding to actually get the repair done in a reasonable amount of time.</p>
