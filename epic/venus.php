93d09662fa87e0df34cf2f00731544efd7f63045
Tanisha Purnell <tanishad93@gmail.com>
2017-10-27 11:16:15 -0700

    updating venus.php

--- epic/venus.php
+++ epic/venus.php
@@ -6,23 +6,26 @@
 	<body>
 		<h1>Purpose, Audience and Goal</h1>
 		<ul>
-			<li><strong>Purpose:</strong> </li>
+			<li><strong>Purpose:</strong>This project aims to serve as a medium where Albuquerque residents can report infrastructure incidents
+				 to city officials and utility workers so that they can resolve them.</li>
 			<br>
-			<li><strong>Audience:</strong> </li>
+			<li><strong>Audience:</strong>The audience of this project will be utility workers, city officials and Albuquerque residents.</li>
 			<br>
-			<li><strong>Goal:</strong> </strong></li>
+			<li><strong>Goal:</strong>This project will create a platform for Albuquerque residents to submit reports of incidents around Albuquerque to city officials and utility workers.</strong></li>
 		</ul>
 		<h3>Persona</h3>
 		<img src ="images/issa-rae-edit.jpg" width="400" height="400" alt="Issa Rae">
 		<ul>
 			<li><strong>Name:</strong> Venus Bradley</li>
 			<li><strong>Age:</strong> 37</li>
+			<li><strong>Qoute:</strong> "I need to get this incident report resolved"</li>
 			<br>
-			<li><strong>Profession:</strong> </li>
+			<li><strong>Profession: City of Albuquerque Infrastructure Manager who is responsible for and manages incoming 311
+							incident reports by local residents so that she can forwarded to respective departments that handle specific incidents.</strong> </li>
 			<br>
-			<li><strong>Technology:</strong> </li>
+			<li><strong>Technology: Dell Desktop, iPad tablet, Dell Laptop, LG</strong> </li>
 			<br>
-			<li><strong>Attitudes and Behaviors:</strong></li>
+			<li><strong>Attitudes and Behaviors: She is detail-oriented and attentive while off/on the job. </strong></li>
 			<br>
 			<li><strong>Frustrations and Needs:</strong>   </li>
 			<br>
