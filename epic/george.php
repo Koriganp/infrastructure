d8dd818d57f6752c1201e345bb813d9809b1baec
Korigan Payne <koriganp@gmail.com>
2017-10-28 16:39:33 -0600

    repaired persona 1

--- epic/george.php
+++ epic/george.php
@@ -17,7 +17,7 @@
 			</nav>
 		</header>
 		<main>
-			<h1 class="header">Persona 1</h1>
+			<h1>Persona 1</h1>
 			<img class="persona" src="images/george.jpg" alt="Image of George Smith"/>
 			<h2 class="about">George Smith</h2>
 			<p>George is 28 single and focused on his career as a marketing director. He has a long commute to his office downtown because he got a condo near the edge of town where the price of homes is cheaper. He likes to go out on the weekends and spends his free time working out, reading, watching movies, and surfing the web.</p>
