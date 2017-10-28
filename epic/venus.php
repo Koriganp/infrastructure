1c92895daf1fbf29d45926dcdc2cbad68f90d788
Korigan Payne <koriganp@gmail.com>
2017-10-28 17:08:04 -0600

    added more to persona 2

--- epic/venus.php
+++ epic/venus.php
@@ -20,12 +20,11 @@
 			<h1>Persona 2</h1>
 			<img class = "persona" src = "images/issa-rae-edit.jpg" alt = "Image of Venus Bradley">
 			<h2 class = "about">Venus Bradley</h2>
-			<p>Venus, 37, is the Albuquerque Infrastructure Manager who is responsible managing all incoming 311
-				reports by local residents so that she can forward specific incidents to their respective departments
-				that resolve and handle incidents by assigned category. She is detail-oriented and attentive while off/on
-				the job.</p>
+			<p>Venus, 37, is the Albuquerque Infrastructure Manager who is responsible managing all incoming 311 reports by local residents. She forwards specific incidents to teh respective department that resolves and handles an incident by its assigned category. She is detail-oriented and attentive while off/on the job.</p>
+			<p>Mrs. Bradley is a single mom of two teenage boys. She loves that her job doesn't make her work late because she really enjoys spending time with her two sons, who are both on the varsity basketball team. She enjoys going to their games and is very proud of how their relationship has improved since they have been playing on the same team.</p>
+			<p>Venus got her business degree going to school on weeknights and weekends, and taking some classes online. She still enjoys learning, though it is now not for a degree. She is very disciplined, always on time, and has accumulated a large amount of sick time because she never calls in sick unless she is admitted into the hospital, which has only happened once.</p>
 			<h2>User Tech</h2>
-			<p class = "story">Venus uses a Dell Desktop, iPad tablet, Dell Laptop, and an LG smartphone.</p>
+			<p class = "story">Venus uses a Dell Desktop at work and has an iPad tablet, Dell Laptop, and an LG smartphone.</p>
 			<h2>Frustrations</h2>
 			<div class = "centerList">
 				<ul>
