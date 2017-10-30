7431601d17e46fae4643281a2937f16b58d09122
Kevin Atkins <kaitoxas@gmail.com>
2017-10-30 09:29:42 -0600

    replaced content that gives context to persona's occupation and frustrations

--- epic/venus.php
+++ epic/venus.php
@@ -16,26 +16,26 @@
 				<a href="conceptual-model.php">Conceptual Model</a>
 			</nav>
 		</header>
-		<main>
-			<h1>Persona 2</h1>
-			<img class = "persona" src = "images/issa-rae-edit.jpg" alt = "Image of Venus Bradley">
-			<h2 class = "about">Venus Bradley</h2>
-			<p>Venus, 37, is the Albuquerque Infrastructure Manager who is responsible managing all incoming 311 reports by local residents. She forwards specific incidents to teh respective department that resolves and handles an incident by its assigned category. She is detail-oriented and attentive while off/on the job.</p>
-			<p>Mrs. Bradley is a single mom of two teenage boys. She loves that her job doesn't make her work late because she really enjoys spending time with her two sons, who are both on the varsity basketball team. She enjoys going to their games and is very proud of how their relationship has improved since they have been playing on the same team.</p>
-			<p>Venus got her business degree going to school on weeknights and weekends, and taking some classes online. She still enjoys learning, though it is now not for a degree. She is very disciplined, always on time, and has accumulated a large amount of sick time because she never calls in sick unless she is admitted into the hospital, which has only happened once in her five years as Infrastructure Manager.</p>
-			<h2>User Tech</h2>
-			<p class = "story">Venus uses a Dell Desktop at work and has an iPad, Dell Laptop, and an LG smartphone.</p>
-			<h2>Frustrations</h2>
-			<div class = "centerList">
-				<ul>
-					<li>Unsorted Incident Reports</li>
-					<li>Lack of priority in determining an incident's urgency.</li>
-					<li>Not knowing what categories are least/most reported.</li>
-				</ul>
-			</div>
-			<h2>User Story</h2>
-				<p class="story">Venus believes that the current medium/web application could be improved
-					so that she can easily manage incident reports.</p>
-		</main>
+		<h1 class = "header">Persona 2</h1>
+		<img class = "persona" src = "images/issa-rae-edit.jpg" width="400" height="400" alt = "Issa Rae">
+		<h2 class = "about">Venus Bradley</h2>
+		<p>Venus, 37, is the Albuquerque Infrastructure Manager who is responsible for managing all incoming 311
+			reports by local residents so that she can forward specific incidents to their respective departments
+			that resolve and handle incidents by assigned category. She is detail-oriented and attentive while off/on
+			the job. She has a Masters' in Civil Engineering and applies her knowledge to further the betterment of the
+			city's infrastructure.</p>
+		<h2>User Tech</h2>
+		<p class = "story"><strong>Technology:</strong> Dell Desktop, iPad, Dell Laptop, LG smartphone</p>
+		<h2>Frustrations</h2>
+		<div class = "centerList">
+			<ul>
+				<li>Unsorted Incident Reports are overwhelming</li>
+				<li>Wishes medium/application had a 'gauge' feature to determine an incident's urgency.</li>
+				<li>Would like an graphical chart that shows percentages of what categories are least/most reported.</li>
+			</ul>
+			<h3>User Story</h3>
+			<p><strong>User Story:</strong> Venus believes that the current medium/web application could be improved
+				so that she can easily manage incident reports.</p>
+		</div>
 	</body>
 </html>
