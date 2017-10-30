dc98fc623409b54e26ba8a2cd316250c86982e3c
Korigan Payne <koriganp@gmail.com>
2017-10-30 11:32:06 -0600

    Added navbar to index page

--- epic/index.php
+++ epic/index.php
@@ -1,7 +1,23 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: korig
- * Date: 10/27/2017
- * Time: 9:47 AM
- */
+<!DOCTYPE html>
+<html lang="en-US">
+	<head>
+		<meta charset="utf-8"/>
+		<meta name="description" content="second persona for capstone"/>
+		<link href="styles/persona.css" rel="stylesheet" type="text/css"/>
+		<link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet"/>
+		<title>Infrastructure Manager User Persona</title>
+	</head>
+	<body>
+		<header>
+			<nav class="topnav">
+				<a href="george.php">Persona 1</a>
+				<a href="venus.php">Persona 2</a>
+				<a href="use-cases.php">Use Cases</a>
+				<a href="conceptual-model.php">Conceptual Model</a>
+			</nav>
+		</header>
+		<main>
+
+		</main>
+	</body>
+</html>
