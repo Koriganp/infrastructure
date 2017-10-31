bf59286a09067f842cf2cf6a204721217c3c8671
Korigan Payne <koriganp@gmail.com>
2017-10-31 13:56:59 -0600

    started image entity

---
+++ php/classes/Image.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Image entity for Infrastructure
+ *
+ * This is the image entity that stores the images that are uploaded with reports.
+ *
+ * @author Korigan Payne <kpayne11@cnm.edu>
+ * @version 7.1
+ */
