2f4c3ee30c474ab9d97372c910a91d02acb0c520
Korigan Payne <koriganp@gmail.com>
2017-10-31 16:58:00 -0600

    updated sql

--- sql/abqreport.sql
+++ sql/abqreport.sql
@@ -1,8 +1,9 @@
-DROP TABLE IF EXISTS `profile`;
-DROP TABLE IF EXISTS category;
-DROP TABLE IF EXISTS report;
-DROP TABLE IF EXISTS image;
-DROP TABLE IF EXISTS `comment`;
+-- DROP TABLE IF EXISTS `comment`;
+-- DROP TABLE IF EXISTS image;
+-- DROP TABLE IF EXISTS report;
+-- DROP TABLE IF EXISTS category;
+-- DROP TABLE IF EXISTS `profile`;
+
 
 CREATE TABLE profile (
 	profileId BINARY(16) NOT NULL ,
