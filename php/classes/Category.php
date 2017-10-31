0ed97ad453732286b4f32abc2b7ecb2840477a4e
Korigan Payne <koriganp@gmail.com>
2017-10-31 12:04:00 -0600

    added accessor and mutator methods to category class

--- php/classes/Category.php
+++ php/classes/Category.php
@@ -53,10 +53,67 @@ class Category implements \JsonSerializable {
 		}
 	}
 
+	/**
+	 * accessor method for category id
+	 *
+	 * @return Uuid value of category id
+	 **/
+	public function getCategoryId() : Uuid {
+		return $this->categoryId;
+	}
+
+	/**
+	 * mutator method for category id
+	 *
+	 * @param Uuid $newCategoryId new value of category id
+	 * @throws \UnexpectedValueException if $newCategoryId is not a uuid
+	 **/
+	public function setCategoryId($newCategoryId) : void {
+		try {
+			$uuid = self::validateUuid($newCategoryId);
+		} catch(\InvalidArgumentException | \RangeException | \Exception | \TypeError $exception){
+			$exceptionType = get_class($exception);
+			throw(new $exceptionType($exception->getMessage(), 0, $exception));
+		}
+		//convert and store the category id
+		$this->categoryId = $uuid;
+	}
+
+	/**
+	 * accessor method for category name
+	 *
+	 * @return string value of category name
+	 **/
+	public function getCategoryName(): string {
+		return ($this->categoryName);
+	}
+
+	/**
+	 * mutator method for category name
+	 *
+	 * @param string $newCategoryName new value of category name
+	 * @throws \InvalidArgumentException if $newCategoryName is not a string or insecure
+	 * @throws \RangeException if $newCategoryName is > 32 characters
+	 * @throws \TypeError if $newCategoryName is not a string
+	 **/
+	public function setCategoryName(string $newCategoryName) : void {
+		// verify the category name is secure
+		$newCategoryName = trim($newCategoryName);
+		$newCategoryName = filter_var($newCategoryName, FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+		if(empty($newCategoryName) === true) {
+			throw(new \InvalidArgumentException("category name is empty or insecure"));
+		}
+		// verify the category name will fit in the database
+		if(strlen($newCategoryName) > 32) {
+			throw(new \RangeException("category name is too large"));
+		}
+		// store the category name
+		$this->categoryName = $newCategoryName;
+	}
+
 	public function jsonSerialize() {
-		// TODO: Implement jsonSerialize() method.
 		$fields = get_object_vars($this);
 		$fields["categoryId"] = $this->categoryId->toString();
-		$fields["categoryName"] = $this->categoryName->toString();
+		$fields["categoryName"] = $this->categoryName->string();
 	}
 }
