f896cf0fc5c78343236fcba9356b11fad8586412
Korigan Payne <koriganp@gmail.com>
2017-11-01 16:04:19 -0600

    started comment test

--- php/classes/Test/CommentTest.php
+++ php/classes/Test/CommentTest.php
@@ -1,5 +1,19 @@
 <?php
 namespace Edu\Cnm\Infrastructure\Test;
-use Edu\Cnm\DataDesign\{Profile, Tweet};
+use Edu\Cnm\DataDesign\{Profile, Report, Comment};
 // grab the class under scrutiny
 require_once(dirname(__DIR__) . "/autoload.php");
+require_once(dirname(__DIR__, 2) . )
+
+/**
+ * Full PHPUnit test for the Comment class
+ *
+ * This is a complete PHPUnit test of the Comment class. It is complete because *ALL* mySQL/PDO enabled methods
+ * are tested for both invalid and valid inputs.
+ *
+ * @see Comment
+ * @author <>
+ **/
+class CommentTest extends InfrastructureTest {
+
+}
