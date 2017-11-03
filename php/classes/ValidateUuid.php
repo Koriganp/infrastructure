c74f973fd63c2a094bc1fc0b5655796d2d689b38
Korigan Payne <koriganp@gmail.com>
2017-11-03 09:24:57 -0600

    fixed path

--- php/classes/ValidateUuid.php
+++ php/classes/ValidateUuid.php
@@ -1,6 +1,6 @@
 <?php
 namespace Edu\Cnm\Infrastructure;
-require_once(dirname(__DIR__, 2) . "../vendor/autoload.php");
+require_once(dirname(__DIR__, 2) . "/vendor/autoload.php");
 use Ramsey\Uuid\Uuid;
 /**
  * Trait to validate a uuid
