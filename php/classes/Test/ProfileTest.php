e4581adce7f77681d893ba730ee983ddf58b3c7c
Tanisha Purnell <tanishad93@gmail.com>
2017-11-03 13:02:39 -0600

    updated profile.php mutators and accessors

---
+++ php/classes/Test/ProfileTest.php
@@ -0,0 +1,35 @@
+<?php
+///**
+// * Created by PhpStorm.
+// * User: neena
+// * Date: 11/3/17
+// * Time: 11:51 AM
+// */
+//
+//namespace Edu\Cnm\Infrastructure\Test;
+//use Edu\Cnm\Infrastructure\{Profile};
+//// grab the class under scrutiny
+//require_once(dirname(__DIR__) . "/autoload.php");
+//require_once(dirname(__DIR__, 2) . "/lib/uuid.php");
+//
+///**
+// * Full PHPUnit test for the Profile class
+// *
+// * This is a complete PHPUnit test of the Category class. It is complete because *ALL* mySQL/PDO enabled methods
+// * are tested for both invalid and valid inputs.
+// *
+// * @see Profile
+// * @author Tanisha Purnell <tpurnell@cnm.edu>
+// **/
+//class ProfileTest extends InfrastructureTest {
+//	/**
+//	 * valid name to use
+//	 * @var string $VALID_NAME
+//	 **/
+//	protected $VALID_NAME = "holes in the ground";
+//	/**
+//	 * second valid name to use
+//	 * @var string $VALID_NAME2
+//	 **/
+//	protected $VALID_NAME2 = "water everywhere";
+//
