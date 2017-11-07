e4d356b263a940bdea95b5e1abb397564e813ad5
Korigan Payne <koriganp@gmail.com>
2017-11-06 19:45:25 -0700

    repairing comment

--- php/classes/Comment.php
+++ php/classes/Comment.php
@@ -14,64 +14,64 @@ require_once(dirname(__DIR__, 2) . "/vendor/autoload.php");
 use Ramsey\Uuid\Uuid;
 
 class Comment implements \JsonSerializable {
-    use ValidateDate;
-    use ValidateUuid;
+	use ValidateDate;
+	use ValidateUuid;
 
-    /**
-     * Primary key, Id for comments posted on reports
-     * @var Uuid $commentId
-     **/
-    private $commentId;
+	/**
+	 * Primary key, Id for comments posted on reports
+	 * @var Uuid $commentId
+	 **/
+	private $commentId;
 
-    /**
-     * Foreign key, Id for the profile that posted the comment in question
-     * @var Uuid $commentProfileId
-     **/
-    private $commentProfileId;
+	/**
+	 * Foreign key, Id for the profile that posted the comment in question
+	 * @var Uuid $commentProfileId
+	 **/
+	private $commentProfileId;
 
-    /**
-     * Foreign key, Id for the report that the comment in question was posted on
-     * @var Uuid $commentReportId
-     **/
-    private $commentReportId;
+	/**
+	 * Foreign key, Id for the report that the comment in question was posted on
+	 * @var Uuid $commentReportId
+	 **/
+	private $commentReportId;
 
-    /**
-     * The content of the comment posted by the user profile
-     * @var string $commentContent
-     **/
-    private $commentContent;
+	/**
+	 * The content of the comment posted by the user profile
+	 * @var string $commentContent
+	 **/
+	private $commentContent;
 
-    /**
-     * This is the date and time the comment was posted
-     * @var \DateTime $commentDateTime
-     **/
-    private $commentDateTime;
+	/**
+	 * This is the date and time the comment was posted
+	 * @var \DateTime $commentDateTime
+	 **/
+	private $commentDateTime;
 
-    /**
-     * Constructor for the comment
-     *
-     * @param Uuid $newCommentId id of the comment
-     * @param Uuid $newCommentProfileId id of the profile that posted the comment
-     * @param Uuid $newCommentReportId id of the report the comment was posted on
-     * @param string $newCommentContent the text written in the comment
-     * @param \DateTime|string|null $newCommentDateTime the
-     * @throws \RangeException if data values are out of bounds (e.g., strings too long, negative integers)
-     * @throws \TypeError if data types violate type hints
-     * @throws \Exception if some other exception occurs
-	  * @Documentation https://php.net/manual/en/language.oop5.decon.php
-     **/
-    public function __construct($newCommentId, $newCommentProfileId, $newCommentReportId, string $newCommentContent, $newCommentDateTime = null) {
-       try {
-      	 	$this->setCommentId($newCommentId);
+	/**
+	 * Constructor for the comment
+	 *
+	 * @param Uuid $newCommentId id of the comment
+	 * @param Uuid $newCommentProfileId id of the profile that posted the comment
+	 * @param Uuid $newCommentReportId id of the report the comment was posted on
+	 * @param string $newCommentContent the text written in the comment
+	 * @param \DateTime|string|null $newCommentDateTime the
+	 * @throws \RangeException if data values are out of bounds (e.g., strings too long, negative integers)
+	 * @throws \TypeError if data types violate type hints
+	 * @throws \Exception if some other exception occurs
+	 * @Documentation https://php.net/manual/en/language.oop5.decon.php
+	 **/
+	public function __construct($newCommentId, $newCommentProfileId, $newCommentReportId, string $newCommentContent, $newCommentDateTime = null) {
+		try {
+				$this->setCommentId($newCommentId);
 				$this->setCommentProfileId($newCommentProfileId);
 				$this->setCommentReportId($newCommentReportId);
 				$this->setCommentContent($newCommentContent);
-				$this->setCommentDate($newCommentDate);
+				$this->setCommentDateTime($newCommentDate);
        } catch (\InvalidArgumentException | \RangeException | \Exception | \TypeError $exception) {
            $exceptionType = get_class($exception);
            throw(new $exceptionType($exception->getMessage(), 0, $exception));
        }
-    }
+	}
 
     /**
      * accessor method for commentId
