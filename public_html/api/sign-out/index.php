9cb5d4a0b8553ce934298c4a285518a7124ca7bc
Kevin Atkins <kaitoxas@gmail.com>
2017-11-12 20:06:17 -0700

    added xsrf challenge & default error message

--- public_html/api/sign-out/index.php
+++ public_html/api/sign-out/index.php
@@ -4,3 +4,14 @@
  *
  * @author Kevin D. Atkins
  */
+
+
+//verify the xsrf challenge
+if(session_status() !== PHP_SESSION_ACTIVE){
+	session_start();
+}
+
+//prepare default error message
+$reply = new stdClass();
+$reply->status = 200;
+$reply->data = null;
