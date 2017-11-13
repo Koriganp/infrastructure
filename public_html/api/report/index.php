19dd4bb000d7aeafe488e0cb31eeefbb5045a250
Kevin Atkins <kaitoxas@gmail.com>
2017-11-13 15:11:16 -0700

    working on assigned APIs

--- public_html/api/report/index.php
+++ public_html/api/report/index.php
@@ -5,6 +5,16 @@
  * @author Kevin D. Atkins
  */
 
+require_once dirname(__DIR__, 3) . "/vendor/autoload.php";
+require_once dirname(__DIR__, 3) . "/php/classes/autoload.php";
+require_once dirname(__DIR__, 3) . "/php/lib/xsrf.php";
+require_once(dirname(__DIR__, 3) . "/php/lib/uuid.php");
+require_once("/etc/apache2/capstone-mysql/encrypted-config.php");
+
+use Edu\Cnm\Infrastructure\ {
+	Report, Category, Image
+};
+
 //verify the xsrf challenge
 if(session_status() !== PHP_SESSION_ACTIVE){
 	session_start();
@@ -14,3 +24,9 @@ if(session_status() !== PHP_SESSION_ACTIVE){
 $reply = new stdClass();
 $reply->status = 200;
 $reply->data = null;
+
+try {
+
+} catch (\Exception | \TypeError $exception) {
+
+}
--- public_html/api/sign-out/index.php
+++ public_html/api/sign-out/index.php
@@ -4,6 +4,11 @@
  *
  * @author Kevin D. Atkins
  */
+require_once dirname(__DIR__, 3) . "/vendor/autoload.php";
+require_once dirname(__DIR__, 3) . "/php/classes/autoload.php";
+require_once dirname(__DIR__, 3) . "/php/lib/xsrf.php";
+require_once(dirname(__DIR__, 3) . "/php/lib/uuid.php");
+// require_once("/etc/apache2/capstone-mysql/encrypted-config.php");
 
 //verify the xsrf challenge
 if(session_status() !== PHP_SESSION_ACTIVE){
@@ -14,3 +19,17 @@ if(session_status() !== PHP_SESSION_ACTIVE){
 $reply = new stdClass();
 $reply->status = 200;
 $reply->data = null;
+
+try {
+	// grab the mySQL connection
+	$pdo = connectToEncryptedMySQL("HTTP_X_HTTP_METHOD", $_SERVER) ? $_SERVER["HTTP_X_HTTP_METHOD"] : $_SERVER["REQUEST_METHOD"];
+
+	// mock a logged in user by forcing the session. For test purposes and not in live code
+
+	//determine which HTTP method was used
+	//$method = array_key_exists("HTTP_");
+
+
+} catch(\Exception | \TypeError $exception) {
+
+}
