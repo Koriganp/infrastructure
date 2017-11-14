f5e2791860670cb87de41ca642288638581a0a61
Jaccuse <guck@jack-arnold.com>
2017-11-14 08:00:33 -0700

    started comment API

---
+++ public_html/api/comment/index.php
@@ -0,0 +1,19 @@
+<?php
+require_once dirname(__DIR__, 3) . "/vendor/autoload.php";
+require_once dirname(__DIR__, 3) . "/php/classes/autoload.php";
+require_once dirname(__DIR__, 3) . "/php/lib/xsrf.php";
+require_once(dirname(__DIR__, 3) . "/php/lib/uuid.php");
+require_once("/etc/apache2/capstone-mysql/encrypted-config.php");
+
+use Edu\Cnm\Infrastructure\{
+    Comment,
+    // we only use the profile class for testing purposes
+    Report
+};
+
+/**
+ * API for the Comment class
+ *
+ * @author Jack Arnold
+ **/
+
