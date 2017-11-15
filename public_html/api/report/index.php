dd3469c19ef04aaba395b7ba419a86a4829987a4
Kevin Atkins <kaitoxas@gmail.com>
2017-11-14 17:50:36 -0700

    made changes to report api

--- public_html/api/report/index.php
+++ public_html/api/report/index.php
@@ -12,7 +12,7 @@ require_once(dirname(__DIR__, 3) . "/php/lib/uuid.php");
 require_once("/etc/apache2/capstone-mysql/encrypted-config.php");
 
 use Edu\Cnm\Infrastructure\ {
-	Report, Category, Comment, Profile
+	Report, Category, Comment, Profile, Image
 };
 
 //verify the xsrf challenge
@@ -25,19 +25,18 @@ $reply = new stdClass();
 $reply->status = 200;
 $reply->data = null;
 
-
 try {
-
 	//grab the mySQL connection
-	$pdo = connectToEncryptedMySQL("/etc/apache2/capstone-mysql/infrastructure.ini");
+	$pdo = connectToEncryptedMySQL("/etc/apache2/capstone-mysql/abqreport.ini");
 
 	// mock a logged in user by forcing the session. For test purposes and not in live code
+
 	//determine which HTTP method was used
 	$method = array_key_exists("HTTP_X_HTTP_METHOD", $_SERVER) ? $_SERVER["HTTP_X_HTTP_METHOD"] : $_SERVER["REQUEST_METHOD"];
 
 	//sanitize input
 	$id = filter_input(INPUT_GET, "id", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
-	$reportCategoryId = filter_input(INPUT_GET, "reportCategoryid", FILTER_VALIDATE_INT);
+	$reportCategoryId = filter_input(INPUT_GET, "reportCategoryid", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
 	$reportContent = filter_input(INPUT_GET, "reportContent", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
 
 	// make sure the id is valid for methods that require it
@@ -54,95 +53,93 @@ try {
 		//get a specific report or all reports and update reply
 		if(empty($id) === false) {
 
+			// grab all the images for that report based on what report it is
+			$image = Image::getImageByImageReportId($pdo, $_SESSION["report"]->getReportId())->toArray();
 			$report = Report::getReportByReportId($pdo, $id);
 			if($report !== null) {
 				$reply->data = $report;
+				$reply->data = $image;
 			}
+
 		} else if(empty($reportCategoryId) === false) {
 
 			// if the category is valid, grab all reports by that category
-			$reports = Report::getReportByReportCategoryId($pdo, $report->getReportByCategoryId()->toArray());
+			$category = Category::getCategoryByCategoryId($pdo, $_SESSION);
+			$reports = Report::getReportByReportCategoryId($pdo, $_SESSION["report"]->getReportByReportCategoryId()->toArray());
 			if($reports !== null) {
 				$reply->data = $reports;
-			} else {
-
-				$reports = Report::getAllReports($pdo)->toArray();
-				if($reports === null) {
-					echo "There are no reports";
-				}
-
-				if($reports !== null) {
-					$reply->data = $reports;
-				}
 			}
-		} else if ($method === "POST") {
-
-			verifyXsrf();
-			$requestContent = file_get_contents("php://input");
-			// Retrieves the JSON package that the front end sent, and stores it in $requestContent. Here we are using file_get_contents("php://input") to get the request from the front end. file_get_contents() is a PHP function that reads a file into a string. The argument for the function, here, is "php://input". This is a read only stream that allows raw data to be read from the front end request which is, in this case, a JSON package.
-			$requestObject = json_decode($requestContent);
-			// This line decodes the JSON package and stores that result in $requestObject
-
-			//make sure report content is available (required field)
-			if(empty($requestObject->reportContent === true)) {
-				throw(new \InvalidArgumentException ("No content for Report.", 405));
-			}
-
-			// make sure report date is accurate (optional field)
-			if(empty($requestObject->reportDateTime) === true) {
-				$requestObject->reportDateTime = date("Y-m-d H:i:s.u");
-			}
-
-			if(empty($_SESSION["category"]) === true) {
-				throw(new \InvalidArgumentException("You must choose a category to submit a report", 403));
-			}
-
-			// create new report and insert into database
-			$report = new Report(generateUuidV4(), $requestObject->getReportCategoryId(), $requestObject->reportContent, $requestObject->reportDateTime, $_SERVER["REMOTE_ADDR"], $requestObject->reportLat, $requestObject->reportLong, $requestObject->reportStatus, $requestObject->reportUrgency, substr($_SERVER["HTTP_USER_AGENT"], 0, 255));
-			$report->insert($pdo);
-
-			// update reply
-			$reply->message = "Report Submitted";
-
-		} else if ($method === "DELETE") {
-
-			//enforce that the end user has a XSRF token.
-			verifyXsrf();
-
-			// retrieve the Report to be deleted
-			$report = Report::getReportByReportId($pdo, $id);
-			if($report === null) {
-
-				throw(new RuntimeException("Report does not exist", 404));
-
-			}
-
-			//enforce the end user has a JWT token
-			//validateJwtHeader();
-
-			//enforce the user is signed in and only trying to delete a report
-			if(empty($_SESSION["profile"]) === true) {
-				throw(new \InvalidArgumentException("You are not allowed to delete this report", 403));
-			}
-
-			// delete report
-			$report->delete($pdo);
-			// update reply
-			$reply->message = "Report was deleted";
 
 		} else {
 
-			throw (new InvalidArgumentException("Invalid HTTP method request", 418));
+			$reports = Report::getAllReports($pdo)->toArray();
+			if($reports === null) {
+				echo "There are no reports";
+			}
+
+			if($reports !== null) {
+				$reply->data = $reports;
+			}
+		}
+	} else if($method === "POST") {
+		verifyXsrf();
+		$requestContent = file_get_contents("php://input");
+		// Retrieves the JSON package that the front end sent, and stores it in $requestContent. Here we are using file_get_contents("php://input") to get the request from the front end. file_get_contents() is a PHP function that reads a file into a string. The argument for the function, here, is "php://input". This is a read only stream that allows raw data to be read from the front end request which is, in this case, a JSON package.
+		$requestObject = json_decode($requestContent);
+		// This line decodes the JSON package and stores that result in $requestObject
+
+		//make sure report content is available (required field)
+		if(empty($requestObject->reportContent === true)) {
+			throw(new \InvalidArgumentException ("No content for Report.", 405));
+		}
+
+		// make sure report date is accurate (optional field)
+		if(empty($requestObject->reportDateTime) === true) {
+			$requestObject->reportDateTime = date("Y-m-d H:i:s.u");
+		}
+
+		if(empty($_SESSION["category"]) === true) {
+			throw(new \InvalidArgumentException("You must choose a category to submit a report", 403));
+		}
+
+		// create new report and insert into database
+		$report = new Report(generateUuidV4(), $requestObject->getReportCategoryId(), $requestObject->reportContent, $requestObject->reportDateTime, $_SERVER["REMOTE_ADDR"], $requestObject->reportLat, $requestObject->reportLong, $requestObject->reportStatus, $requestObject->reportUrgency, substr($_SERVER["HTTP_USER_AGENT"], 0, 255));
+		$report->insert($pdo);
+
+		// update reply
+		$reply->message = "Report Submitted";
+	} else if($method === "DELETE") {
+		//enforce that the end user has a XSRF token.
+		verifyXsrf();
+
+		// retrieve the Report to be deleted
+		$report = Report::getReportByReportId($pdo, $id);
+		if($report === null) {
+
+			throw(new RuntimeException("Report does not exist", 404));
 
 		}
+
+		//enforce the end user has a JWT token
+		//validateJwtHeader();
+
+		//enforce the user is signed in and only trying to delete a report
+		if(empty($_SESSION["profile"]) === true) {
+			throw(new \InvalidArgumentException("You are not allowed to delete this report", 403));
+		}
+
+		// delete report
+		$report->delete($pdo);
+		// update reply
+		$reply->message = "Report was deleted";
+	} else {
+		throw (new InvalidArgumentException("Invalid HTTP method request", 418));
 	}
+} catch(\Exception | \TypeError $exception) {
 
-} catch (\Exception | \TypeError $exception) {
-
-	// update the $reply->status $reply->message
-	$reply->status = $exception->getCode();
-	$reply->message = $exception->getMessage();
-
+		// update the $reply->status $reply->message
+		$reply->status = $exception->getCode();
+		$reply->message = $exception->getMessage();
 }
 
 header("Content-type: application/json");
