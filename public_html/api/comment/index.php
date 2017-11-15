803a41c32f2b118e6a4ebb05c2b0074d91af5b7d
Jaccuse <guck@jack-arnold.com>
2017-11-15 13:42:30 -0700

    started working on try block for comment API

--- public_html/api/comment/index.php
+++ public_html/api/comment/index.php
@@ -8,7 +8,7 @@ require_once("/etc/apache2/capstone-mysql/encrypted-config.php");
 use Edu\Cnm\Infrastructure\{
     Comment,
     // we only use the profile class for testing purposes
-    Report
+    Report, Category
 };
 
 /**
@@ -27,3 +27,30 @@ $reply = new stdClass();
 $reply->status = 200;
 $reply->data = null;
 
+try {
+    $pdo = connectEncryptedMySQL("/etc/apache2/capstone-mysql/abqreport.ini");
+
+    $method = array_key_exists("HTTP_X_HTTP_METHOD", $_SERVER) ? $_SERVER["HTTP_X_HTTP_METHOD"] : $_SERVER["REQUEST_METHOD"];
+
+    $id = filter_input(INPUT_GET, "id", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+    $commentProfileId = filter_input(INPUT_GET, "commentProfileId", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+    $commentReportId = filter_input(INPUT_GET, "commentReportId", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+    $commentContent = filter_input(INPUT_GET, "commentContent", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+
+    if(($method === "DELETE" || $method === "PUT") && (empty($id) === true)) {
+        throw(new InvalidArgumentException("id cannot be empty or negative", 405));
+    }
+
+    if($method === "GET") {
+        setXsrfCookie();
+
+        if(empty($id) === false) {
+            $comment = Comment::getCommentByCommentId($pdo, $id);
+            if($comment !== null) {
+                $reply->data = $comment;
+            } else if(empty($commentProfileId) === false) {
+
+            }
+        }
+    }
+}
