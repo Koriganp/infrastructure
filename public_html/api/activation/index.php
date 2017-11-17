e6e34bccaf17180547133e3c706bc306235f0871
Kevin Atkins <kaitoxas@gmail.com>
2017-11-17 10:09:58 -0700

    https://app.asana.com/0/464075553520411/476682531498903

--- public_html/api/activation/index.php
+++ public_html/api/activation/index.php
@@ -9,6 +9,7 @@ require_once dirname(__DIR__,3)."/php/lib/xsrf.php";
 require_once("/etc/apache2/capstone-mysql/encrypted-config.php");
 use Edu\Cnm\Infrastructure\Profile;
 
+
 //verify the xsrf challenge
 if(session_status() !== PHP_SESSION_ACTIVE){
 	session_start();
