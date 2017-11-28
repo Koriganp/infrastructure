5f1e2884fe70e601a022fd9c33f08ee390927fa7
Korigan Payne <koriganp@gmail.com>
2017-11-28 16:17:02 -0700

    worked on head utils

--- static-ui/head-utils.php
+++ static-ui/head-utils.php
@@ -11,6 +11,10 @@
 
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous"/>
 
+		<!-- Font Awesome -->
+
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"/>
+
 		<!-- Custom CSS -->
 
 		<!-- jQuery, Popper.js, Bootstrap JS -->
