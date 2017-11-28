8aef7216e4f1a11b4ab282b17247e140567cd5cd
Tanisha Purnell <tanishad93@gmail.com>
2017-11-28 16:00:37 -0700

    initial commit for sign-up, sign-in and sign-out static ui

---
+++ static-ui/sign-up.php
@@ -0,0 +1,27 @@
+<form>
+	<div class="form-row">
+		<div class="form-group col-md-6">
+			<label for="inputEmail4">First Name</label>
+			<input type="email" class="form-control" id="inputEmail4" placeholder="First Name">
+		</div>
+		<div class="form-group col-md-6">
+			<label for="inputPassword4">Last Name</label>
+			<input type="password" class="form-control" id="inputPassword4" placeholder="Last Name">
+		</div>
+	</div>
+	<div class="form-group">
+		<label for="inputAddress2">Username</label>
+		<input type="text" class="form-control" id="inputAddress2" placeholder="Username">
+	</div>
+	<div class="form-row">
+		<div class="form-group col-md-6">
+			<label for="inputEmail4">Email</label>
+			<input type="email" class="form-control" id="inputEmail4" placeholder="Email">
+		</div>
+		<div class="form-group col-md-6">
+			<label for="inputPassword4">Password</label>
+			<input type="password" class="form-control" id="inputPassword4" placeholder="Password">
+		</div>
+	</div>
+	<button type="submit" class="btn btn-primary">Submit</button>
+</form>
