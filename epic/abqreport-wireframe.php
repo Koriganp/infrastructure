b8c6dd6216b13af7787022227e9d6a29fd689c2f
Kevin Atkins <kaitoxas@gmail.com>
2017-11-27 18:20:28 -0700

    created abqreport-wireframe.php

---
+++ epic/abqreport-wireframe.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<title>Conceptual Model</title>
+	</head>
+	<body>
+		<img src="images/abqreport0.png" alt="ABQ Report 0"/>
+		<img src="images/abqreport1.png" alt="ABQ Report 1"/>
+	</body>
+</html>
