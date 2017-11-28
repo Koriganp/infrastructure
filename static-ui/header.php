9a8a5d410aaea4b2dcbf680ca5ac3e33e77d7abe
Korigan Payne <koriganp@gmail.com>
2017-11-28 16:14:44 -0700

    Finished header

--- static-ui/header.php
+++ static-ui/header.php
@@ -1,12 +1,23 @@
 <header>
 	<nav class="navbar navbar-default navbar-expand-md">
-		<a class="navbar-brand" href=""></a>
+		<a class="navbar-brand" href="">ABQ Report</a>
 		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
 			<span class="navbar-toggler-icon"></span>
 		</button>
 		<div class="collapse navbar-collapse" id="navbarTogglerDemo01">
 			<ul class="navbar-nav ml-auto">
-<!--	// insert navbar links here as <li><a></a></li>-->
+				<li class="nav-link">
+					<a href="">Admin Dashboard</a>
+				</li>
+				<li class="nav-link">
+					<a href="">Sign Up</a>
+				</li>
+				<li class="nav-link">
+					<a href="">Sign In</a>
+				</li>
+				<li class="nav-link">
+					<a href="">Sign Out</a>
+				</li>
 			</ul>
 		</div>
 	</nav>
