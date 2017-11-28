9b80ac0025a55ce84de1b1f34f4bbc8236158b5c
Korigan Payne <koriganp@gmail.com>
2017-11-28 16:16:21 -0700

    finished footer

--- static-ui/footer.php
+++ static-ui/footer.php
@@ -1,6 +1,6 @@
-		<footer>
-			<div class="footer">
-				<p></p>
+		<footer class="footer">
+			<div class="bg-light text-dark nav justify-content-center p-4">
+				<p>&copy; 2017 ABQ Report</p>
 			</div>
 		</footer>
 	</body>
