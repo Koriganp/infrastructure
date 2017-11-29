57067ff669f4a23288f336825e95f69a762e188b
Kevin Atkins <kaitoxas@gmail.com>
2017-11-28 22:00:40 -0700

    added 'templates/splash.html' & 'templates/infrastructure-app.html'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,8 @@
 import {Component} from "@angular/core";
 
 @Component({
-	selector: "angular5-example-app",
-	templateUrl: "./templates/angular5-example-app.html"
+	selector: "infrastructure-app",
+	templateUrl: "./templates/infrastructure-app.html"
 })
 
 export class AppComponent {}
