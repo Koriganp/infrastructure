fda0b1eb794ad867a446791d323138b2ec64bcd1
Kevin Atkins <kaitoxas@gmail.com>
2017-11-29 23:52:07 -0700

    completed 'report-public-view.php' & 'home-view.php'

--- static-ui/home-view.php
+++ static-ui/home-view.php
@@ -2,14 +2,12 @@
 <!DOCTYPE html>
 <html lang="en-US">
 	<!-- inject head-utils -->
-
 	<body>
 		<!-- inject navbar -->
 
 		<!-- begin Home Page layout -->
 		<main>
 			<div class="container-fluid">
-
 
 			</div>
 		</main>
