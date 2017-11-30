ddb5101a2fd3aa3613d4bd53ad61f168262b83ee
Kevin Atkins <kaitoxas@gmail.com>
2017-11-29 22:50:02 -0700

    completed 'admin-dashboard.php'

--- static-ui/admin-dashboard.php
+++ static-ui/admin-dashboard.php
@@ -1,1 +1,18 @@
 <!-- Admin Dashboard Page-->
+<!DOCTYPE html>
+<html lang="en-US">
+	<!-- inject head-utils -->
+	<body>
+		<!-- inject navbar -->
+
+		<!-- begin Home Page layout -->
+		<main>
+			<div class="container-fluid">
+
+			</div>
+
+		</main>
+
+	</body>
+
+</html>
