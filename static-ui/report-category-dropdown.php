083cef7bbfa60708c4bf041cfec0b29cf17af645
Jaccuse <guck@jack-arnold.com>
2017-11-29 23:19:02 -0700

    Finished report category dropdown to the extent of my knowledge of how to do so

---
+++ static-ui/report-category-dropdown.php
@@ -0,0 +1,31 @@
+<!-- Dropdown for report categories which goes on the admin dashboard page I think? -->
+<!-- From what I can tell <!DOCTYPE html> is not required? -->
+<div class="dropdown">
+    <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+
+    </button>
+    <div class="dropdown-menu" aria-labelledby="dropdownMenu1">
+        <a class="dropdown-item" href="#!">
+
+        </a>
+        <a class="dropdown-item" href="#!">
+
+        </a>
+        <a class="dropdown-item" href="#!">
+
+        </a>
+        <a class="dropdown-item" href="#!">
+
+        </a>
+        <a class="dropdown-item" href="#!">
+
+        </a>
+        <a class="dropdown-item" href="#!">
+
+        </a>
+        <a class="dropdown-item" href="#!">
+
+        </a>
+    </div>
+</div>
+<!-- I have no idea if I did this even remotely correctly -->
