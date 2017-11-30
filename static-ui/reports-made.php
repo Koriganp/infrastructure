b1a8082bd3bce2782214dee0c5938ad6a4ae9f3c
Kevin Atkins <kaitoxas@gmail.com>
2017-11-29 23:27:34 -0700

    completed 'reports-made.php'

--- static-ui/reports-made.php
+++ static-ui/reports-made.php
@@ -1,1 +1,19 @@
 <!-- Reports Made/History List (Home View Partial)-->
+<table class="table">
+	<thead>
+		<tr>
+			<th scope="col-4">Category</th>
+			<th scope="col-2">Date Reported</th>
+			<th scope="col-2">Report Status</th>
+			<th scope="col-4">Report Content</th>
+		</tr>
+	</thead>
+	<tbody>
+		<tr>
+			<th scope="row">Traffic Signs & Signals</th>
+			<td>11/27/2017</td>
+			<td>Reported</td>
+			<td>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ac nunc ut nisi mollis commodo ut ac dolor. Suspendisse lectus odio, eleifend a rhoncus quis, pharetra non dui. Suspendisse faucibus ut dui vitae hendrerit. Nullam id ex nec nisi pretium lacinia. Nam lacinia sollicitudin bibendum. Vivamus pellentesque risus tellus, sit amet dignissim diam finibus vel. Aliquam lacus nisi, posuere ut nunc dignissim, congue imperdiet elit. Donec in blandit arcu, sit amet luctus felis. Nulla iaculis libero vitae dolor aliquam dignissim. Sed pretium non enim ac venenatis. Nulla euismod molestie enim, id dignissim eros porta quis. Quisque mauris risus, dictum id arcu quis, aliquet imperdiet orci. Donec eu semper tortor. Quisque in imperdiet velit, volutpat rhoncus justo.</td>
+		</tr>
+	</tbody>
+</table>
