2792b2e007fc8b1f45d5eaa6c826e7c54d118c95
Kevin Atkins <kaitoxas@gmail.com>
2017-11-29 17:17:19 -0700

    completed template 'home-view'

--- static-ui/home-view.php
+++ static-ui/home-view.php
@@ -1,1 +1,19 @@
 <!-- Public/Home Page -->
+<!DOCTYPE html>
+<html lang="en-US">
+	<!-- inject head-utils -->
+
+	<body>
+		<!-- inject navbar -->
+
+		<!-- begin Home Page layout -->
+		<main>
+			<div class="container-fluid">
+
+
+			</div>
+		</main>
+
+	</body>
+
+</html>
