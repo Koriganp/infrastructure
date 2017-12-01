02fc5daae4effd40223baee18a6747db81310ecd
Korigan Payne <koriganp@gmail.com>
2017-12-01 10:43:52 -0700

    added selectors

--- src/app/components/footer.component.ts
+++ src/app/components/footer.component.ts
@@ -2,7 +2,7 @@ import {Component} from "@angular/core";
 
 @Component({
 	templateUrl: "./templates/footer.html",
-	selector: "navbar"
+	selector: "footer"
 })
 
 export class FooterComponent {}
--- src/app/components/home-view.component.ts
+++ src/app/components/home-view.component.ts
@@ -1,7 +1,8 @@
 import {Component} from "@angular/core";
 
 @Component({
-	templateUrl: "./templates/home-view.html"
+	templateUrl: "./templates/home-view.html",
+	selector: "homeview"
 })
 
 export class HomeViewComponent {}
