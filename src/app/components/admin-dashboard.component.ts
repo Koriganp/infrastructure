98418caa6aa269d8d8bfca16c7ff1b2e14b4a2aa
Korigan Payne <koriganp@gmail.com>
2017-12-01 10:46:05 -0700

    added selector to admin dashboard

--- src/app/components/admin-dashboard.component.ts
+++ src/app/components/admin-dashboard.component.ts
@@ -1,7 +1,8 @@
 import {Component} from "@angular/core";
 
 @Component({
-	templateUrl: "./templates/admin-dashboard.html"
+	templateUrl: "./templates/admin-dashboard.html",
+	selector: "admin-dashboard"
 })
 
 export class AdminDashboardComponent {}
