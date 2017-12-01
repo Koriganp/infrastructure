db42775d3f51260c8e91fde8d6823ddd870f6268
Korigan Payne <koriganp@gmail.com>
2017-12-01 10:48:17 -0700

    added selector to report public view

--- src/app/components/report-public-view.component.ts
+++ src/app/components/report-public-view.component.ts
@@ -1,7 +1,8 @@
 import {Component} from "@angular/core";
 
 @Component({
-	templateUrl: "./templates/report-public-view.html"
+	templateUrl: "./templates/report-public-view.html",
+	selector: "report-public-view"
 })
 
 export class ReportPublicViewComponent {}
