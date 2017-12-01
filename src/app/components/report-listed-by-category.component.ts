4fd5f688475485cafcf84a26b06377763ac37042
Jaccuse <guck@jack-arnold.com>
2017-12-01 09:52:21 -0700

    Added report listed by category component

---
+++ src/app/components/report-listed-by-category.component.ts
@@ -0,0 +1,8 @@
+import {Component} from "@angular/core";
+
+@Component({
+    templateUrl: "./templates/report-listed-by-category.html",
+    selector: "report-listed-by-category"
+})
+
+export class ReportListedByCategoryComponent {}
