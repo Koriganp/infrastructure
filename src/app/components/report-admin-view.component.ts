a71a860d08e7963d7f37bd6ebfb4b8bf2bdb0795
Korigan Payne <koriganp@gmail.com>
2017-12-01 10:47:11 -0700

    added selector to report admin view

--- src/app/components/report-admin-view.component.ts
+++ src/app/components/report-admin-view.component.ts
@@ -1,7 +1,8 @@
 import {Component} from "@angular/core";
 
 @Component({
-	templateUrl: "./templates/report-admin-view.html"
+	templateUrl: "./templates/report-admin-view.html",
+	selector: "report-admin-view"
 })
 
 export class ReportAdminViewComponent {}
