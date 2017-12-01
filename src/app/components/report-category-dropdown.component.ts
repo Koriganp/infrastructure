11c3e8e5f1e330a6dc10d33669eee3fb8ed4805e
Jaccuse <guck@jack-arnold.com>
2017-12-01 09:55:57 -0700

    Added report category dropdown component

---
+++ src/app/components/report-category-dropdown.component.ts
@@ -0,0 +1,8 @@
+import {Component} from "@angular/core";
+
+@Component({
+    templateUrl: "./templates/report-category-dropdown.html",
+    selector: "report-category-dropdown"
+})
+
+export class ReportCategoryDropdownComponent {}
