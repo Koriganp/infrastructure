7e484c53595aebee128320c53e53a6cfaeff333e
Tanisha Purnell <tanishad93@gmail.com>
2017-12-02 14:06:38 -0700

    updated profile.services.ts

--- src/app/services/profile.services.ts
+++ src/app/services/profile.services.ts
@@ -6,9 +6,9 @@ import {Profile} from "../classes/profile";
 import {Observable} from "rxjs/Observable";
 
 @Injectable ()
-export class ProfileService  {
+export class ProfileService {
 
-	constructor(protected http : HttpClient) {
+	constructor(protected http: HttpClient) {
 
 	}
 
@@ -16,29 +16,35 @@ export class ProfileService  {
 	private profileUrl = "api/profile/";
 
 	//reach out to the profile  API and delete the profile in question
-	deleteProfile(id : number) : Observable<Status> {
-		return(this.http.delete<Status>(this.profileUrl + id));
+	deleteProfile(id: number): Observable<Status> {
+		return (this.http.delete<Status>(this.profileUrl + id));
 	}
 
 	// call to the Profile API and edit the profile in question
-	editProfile(profile: Profile) : Observable<Status> {
-		return(this.http.put<Status>(this.profileUrl , profile));
+	editProfile(profile: Profile): Observable<Status> {
+		return (this.http.put<Status>(this.profileUrl, profile));
 	}
 
 	// call to the Profile API and get a Profile object by its id
-	getProfile(id: number) : Observable<Profile> {
-		return(this.http.get<Profile>(this.profileUrl + id));
-
-	}
-
-	// call to the API to grab an array of profiles based on the user input
-	getProfileByProfileUsername(profileUsername: string) :Observable<Profile[]> {
-		return(this.http.get<Profile[]>(this.profileUrl + "?profileUsername=" + profileUsername));
+	getProfile(id: number): Observable<Profile> {
+		return (this.http.get<Profile>(this.profileUrl + id));
 
 	}
 
 	//call to the profile API and grab the corresponding profile by its email
-	getProfileByProfileEmail(profileEmail: string) :Observable<Profile[]> {
-		return(this.http.get<Profile[]>(this.profileUrl + "?profileEmail=" + profileEmail));
+	getProfileByProfileEmail(profileEmail: string): Observable<Profile[]> {
+		return (this.http.get<Profile[]>(this.profileUrl + "?profileEmail=" + profileEmail));
 	}
+
+	// call to the API to grab an array of profiles based on the user input
+	getProfileByProfileUsername(profileUsername: string): Observable<Profile[]> {
+		return (this.http.get<Profile[]>(this.profileUrl + "?profileUsername=" + profileUsername));
+
+	}
+
+	// call to the API to grab an array of profiles based on the user input
+	getProfileByActivationToken(profileActivationToken: string): Observable<Profile[]> {
+		return (this.http.get<Profile[]>(this.profileUrl + "?profileActivationToken=" + profileActivationToken));
+	}
+
 }
