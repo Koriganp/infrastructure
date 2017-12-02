96aad5b6b26fd1ad096f7cc666b06a54f61c40c0
Korigan Payne <koriganp@gmail.com>
2017-12-02 13:06:33 -0700

    edited app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,18 @@
 import {Component} from "@angular/core";
+import {SessionService} from "./services/session.service";
+import {Status} from "./classes/status";
 
 @Component({
 	selector: "home-view",
 	templateUrl: "./templates/home-view.html"
 })
 
-export class AppComponent {}
+export class AppComponent {
+
+	status : Status = null;
+
+	constructor(protected sessionService : SessionService) {
+		this.sessionService.setSession()
+			.subscribe(status => this.status = status);
+	}
+}
