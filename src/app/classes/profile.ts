8c2d7403696b882c62dc78c4edc7292fbc96f785
Tanisha Purnell <tanishad93@gmail.com>
2017-12-02 13:43:21 -0700

    Created Profile Class and src files

---
+++ src/app/classes/profile.ts
@@ -0,0 +1,9 @@
+export class Profile {
+	constructor(
+		public profileId: string,
+		public profileUsername: string,
+		public profileEmail: string,
+		public profilePassword: string,
+		public profilePasswordConfirm: string
+	) {}
+}
