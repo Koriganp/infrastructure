6026aed17fe108c601fc634d421bc334841d36e0
Kevin Atkins <kaitoxas@gmail.com>
2017-12-05 09:57:25 -0700

    updated api 'report'

--- public_html/api/report/index.php
+++ public_html/api/report/index.php
@@ -8,6 +8,7 @@
 require_once(dirname(__DIR__, 3) . "/vendor/autoload.php");
 require_once(dirname(__DIR__, 3) . "/php/classes/autoload.php");
 require_once(dirname(__DIR__, 3) . "/php/lib/xsrf.php");
+require_once(dirname(__DIR__, 3)) . "php/lib/geocode.php";
 require_once(dirname(__DIR__, 3) . "/php/lib/uuid.php");
 require_once(dirname(__DIR__, 3) . "/php/lib/jwt.php");
 require_once("/etc/apache2/capstone-mysql/encrypted-config.php");
