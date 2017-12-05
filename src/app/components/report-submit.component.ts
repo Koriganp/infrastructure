3ec4632c9f9b39edf2b5b62dc2ae12cab63e4db9
Kevin Atkins <kaitoxas@gmail.com>
2017-12-05 13:45:11 -0700

    updated 'report-submit.component.ts'

--- src/app/components/report-submit.component.ts
+++ src/app/components/report-submit.component.ts
@@ -1,8 +1,24 @@
 import {Component} from "@angular/core";
+import {ReportService} from "../services/report.service";
+import {Status} from "../classes/status";
+import {Report} from "../classes/report";
 
 @Component({
 	selector: "report-submit",
 	templateUrl: "./templates/report-submit.html",
 })
 
-export class ReportSubmitComponent {}
+export class ReportSubmitComponent {
+
+	//declare needed state variables for later use.
+	status: Status = null;
+
+	report: Report = new Report(null, null, null, null, null, null, null, null, null,);
+
+	constructor(private reportService : ReportService) {}
+
+	createReport(): void {
+		this.reportService.createReport(this.report)
+			.subscribe(status => this.status = this.status);
+	}
+}
