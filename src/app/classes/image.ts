b22046c77fd78a14d76b04b48ca735ea9eb33aee
Kevin Atkins <kaitoxas@gmail.com>
2017-12-05 16:01:43 -0700

    made 'image' class

---
+++ src/app/classes/image.ts
@@ -0,0 +1,9 @@
+export class Image {
+	constructor(
+		public imageId: string,
+		public imageReportId: string,
+		public imageCloudinary: string,
+		public imageLat: number,
+		public imageLong: number
+	){}
+}
