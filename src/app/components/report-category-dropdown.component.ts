80349a4e0537eed15421c8784a2a30f9f4c1d6f7
Korigan Payne <koriganp@gmail.com>
2017-12-05 16:08:56 -0700

    completed report category dropdown

--- src/app/components/report-category-dropdown.component.ts
+++ src/app/components/report-category-dropdown.component.ts
@@ -1,8 +1,22 @@
-import {Component} from "@angular/core";
+import {Component, OnInit} from "@angular/core";
+import {CategoryService} from "../services/category.service";
+import {Category} from "../classes/category";
 
 @Component({
-    templateUrl: "./templates/report-category-dropdown.html",
-    selector: "report-category-dropdown"
+	templateUrl: "./templates/report-category-dropdown.html",
+	selector: "report-category-dropdown"
 })
 
-export class ReportCategoryDropdownComponent {}
+export class ReportCategoryDropdownComponent implements OnInit {
+	categories: Category[] = [];
+	constructor(
+		private categoryService: CategoryService,
+	) {}
+
+	ngOnInit() : void {
+		this.categoryService.getAllCategories()
+			.subscribe(categories => this.categories = categories);
+	}
+
+
+}
