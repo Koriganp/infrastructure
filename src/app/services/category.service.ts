0da51be8e9ec5393bc88521bf0d4956f8738c634
Korigan Payne <koriganp@gmail.com>
2017-12-05 11:20:36 -0700

    removed unused import statement

--- src/app/services/category.service.ts
+++ src/app/services/category.service.ts
@@ -1,7 +1,5 @@
 import {Injectable} from "@angular/core";
 import {HttpClient} from "@angular/common/http";
-
-import {Status} from "../classes/status";
 import {Category} from "../classes/category";
 import {Observable} from "rxjs/Observable";
 
