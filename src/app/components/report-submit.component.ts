3ed3fe04119cd848c58028d17dc33eb1d6c303da
Kevin Atkins <kaitoxas@gmail.com>
2017-12-06 12:10:32 -0700

    took out imported 'ActivatedRoute' & 'Params'

--- src/app/components/report-submit.component.ts
+++ src/app/components/report-submit.component.ts
@@ -1,6 +1,5 @@
 import {Component, OnInit} from "@angular/core";
 import {FormBuilder, FormGroup, Validators} from "@angular/forms";
-import {ActivatedRoute, Params} from "@angular/router";
 
 import {Status} from "../classes/status";
 import {Report} from "../classes/report";
