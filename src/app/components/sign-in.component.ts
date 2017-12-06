717d6fb6bb343388c38acbc8de699c2c3c8dfee0
Korigan Payne <koriganp@gmail.com>
2017-12-06 13:21:10 -0700

    finished sign-in

--- src/app/components/sign-in.component.ts
+++ src/app/components/sign-in.component.ts
@@ -1,44 +1,62 @@
 //this component controls the sign-in modal when "sign-in" is clicked
-import {Component, ViewChild} from "@angular/core";
-
-
-
+import {Component, OnInit, ViewChild} from "@angular/core";
 import {Router} from "@angular/router";
 import {Status} from "../classes/status";
 import {SignInService} from "../services/sign.in.service";
 import {SignIn} from "../classes/sign.in";
 import {CookieService} from "ng2-cookies";
-declare let $: any;
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {SessionService} from "../services/session.service";
+declare const $: any;
 
 @Component({
 	templateUrl: "./templates/sign-in.html",
 	selector: "sign-in"
 })
 
-export class SignInComponent {
-	@ViewChild("signInForm") signInForm: any;
+export class SignInComponent implements OnInit {
+	@ViewChild("signInForm")
 
-	signIn: SignIn = new SignIn(null, null);
+	signInForm: FormGroup;
+	signin: SignIn = new SignIn(null, null);
 	status: Status = null;
 	//cookie: any = {};
-	constructor(private signInService: SignInService, private router: Router, private cookieService : CookieService) {
+
+	constructor(
+		private signInService: SignInService,
+		private sessionService: SessionService,
+		private formBuilder: FormBuilder,
+		private router: Router,
+		private cookieService : CookieService) {}
+
+	ngOnInit() : void {
+		this.signInForm = this.formBuilder.group({
+			profileEmail: ["", [Validators.maxLength(128), Validators.required]],
+			profilePassword: ["", [Validators.maxLength(128), Validators.required]]
+		});
+		this.applyFormChanges();
 	}
 
-
-
-	createSignIn(): void {
-		this.signInService.postSignIn(this.signIn).subscribe(status => {
-			this.status = status;
-
-			if(status.status === 200) {
-
-				this.router.navigate([""]);
-				//location.reload(true);
-				this.signInForm.reset();
-				setTimeout(function(){$("#signin-modal").modal('hide');},1000);
-			} else {
-				console.log("failed login")
+	applyFormChanges() : void {
+		this.signInForm.valueChanges.subscribe(values => {
+			for(let field in values) {
+				this.signin[field] = values[field];
 			}
 		});
+	}
+
+	signIn() : void {
+		this.signInService.postSignIn(this.signin)
+			.subscribe(status => {
+				this.status = status;
+				if(this.status.status === 200) {
+					this.sessionService.setSession();
+					this.signInForm.reset();
+					this.router.navigate(["admin-dashboard"]);
+					console.log("signin successful");
+				} else {
+					console.log("failed login");
+				}
+			});
 	}
 }
