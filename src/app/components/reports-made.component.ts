b7c947f21e5456c9ff73eb4481a3b64cecb24cd4
Jaccuse <guck@jack-arnold.com>
2017-12-06 16:09:32 -0700

    Added reports made (lists all reports)

--- src/app/components/reports-made.component.ts
+++ src/app/components/reports-made.component.ts
@@ -1,7 +1,48 @@
-import {Component} from "@angular/core";
+import {Component, OnInit} from "@angular/core";
+
+import {AuthService} from "../services/auth.service";
+import {ReportService} from "../services/report.service";
+import {CategoryService} from "../services/category.service";
+import {ProfileService} from "../services/profile.services";
+
+import {Report} from "../classes/report";
+import {Category} from "../classes/category";
+import {Status} from "../classes/status";
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 
 @Component({
-	templateUrl: "./templates/reports-made.html",
+    templateUrl: "./templates/report-listed-by-category.html",
+    selector: "report-listed-by-category"
 })
 
-export class ReportsMadeComponent {}
+export class ReportListedByCategoryComponent implements OnInit {
+
+    reportListedByCategoryForm: FormGroup;
+
+    status : Status = null;
+
+    category : Category = new Category(null, null);
+
+    report : Report = new Report(null, null, null, null, null, null, null);
+
+    reports: Report[] = [];
+
+    constructor(private authService : AuthService, private formBuilder : FormBuilder, private reportService : ReportService, private categoryService : CategoryService, private profileService : ProfileService) {}
+
+    // life cycling before george's eyes
+    ngOnInit() : void {
+        this.listAllReports();
+
+        this.reportListedByCategoryForm = this.formBuilder.group({
+
+        });
+    }
+
+    listAllReports() : void {
+        this.reportService.getAllReports()
+            .subscribe(reports => this.reports = reports);
+    }
+
+
+
+}
