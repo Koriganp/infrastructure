b205e4595137c5db6e727392691bc57f79e0d215
Jaccuse <guck@jack-arnold.com>
2017-12-06 14:28:43 -0700

    Added export class and imports

--- src/app/components/report-listed-by-category.component.ts
+++ src/app/components/report-listed-by-category.component.ts
@@ -1,8 +1,27 @@
-import {Component} from "@angular/core";
+import {Component, OnInit} from "@angular/core";
+
+import {AuthService} from "../services/auth.service";
+import {ReportService} from "../services/report.service";
+import {CategoryService} from "../services/category.service";
+
+import {Report} from "../classes/report";
+import {Category} from "../classes/category";
+import {Status} from "../classes/status";
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 
 @Component({
     templateUrl: "./templates/report-listed-by-category.html",
     selector: "report-listed-by-category"
 })
 
-export class ReportListedByCategoryComponent {}
+export class ReportListedByCategoryComponent implements onInit {
+
+    createCommentForm: FormGroup;
+
+    status : Status = new Status(null, null, null);
+
+    category : Category = new Category(null, null);
+
+    report : Report = new Report(null, null, null, null, null, null, null);
+
+}
