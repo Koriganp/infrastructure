0cba275da207233856f270546757954dafc149b5
Tanisha Purnell <tanishad93@gmail.com>
2017-12-06 10:11:27 -0700

    finished the report-admin-view.ts
    dont know wtf I did , but I did it . BlAH

--- src/app/components/report-admin-view.component.ts
+++ src/app/components/report-admin-view.component.ts
@@ -22,16 +22,36 @@ export class ReportAdminViewComponent {
 
 	category: Category = new Category(null, null);
 
-	report: Report = new Report(null, null, null, null, null, null, null);
+	report: Report = new Report(null, null, null, null, null, null, null, null, null);
 
 	image: Image = new Image(null, null, null, null, null);
 
 	comment: Comment = new Comment(null, null, null, null, null);
 
+	profile: Profile = new Profile(null, null, null, null, null)
+
 	//declare needed state variables for later use
 	status: Status = null;
 
-	constructor(private authService: AuthService, private formBuilder: FormBuilder, private profileService: ProfileService, private reportService: ReportService, private imageService: ImageService, private commentService: CommentService) {}
+	constructor(private authService: AuthService, private formBuilder: FormBuilder, private profileService: ProfileService, private reportService: ReportService, private imageService: ImageService, private commentService: CommentService) {
+	}
 
+	getReport(): void {
+		this.reportService.getReport(this.report.reportId)
+	}
+	createReport(): void  {
 
+		let report = new Report(null,null,null,null, null, null, null, this.createReportAdminForm.value.reportContent, null);
+
+		this.reportService.createReport(report)
+			.subscribe(status =>{
+				this.status = status;
+				if(this.status.status ===200) {
+					this.createReport();
+					alert(this.status.message);
+				}
+			});
+	}
 }
+
+
