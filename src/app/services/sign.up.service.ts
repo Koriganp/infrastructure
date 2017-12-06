5c342207ce4d8126c798d84e57164f61ca66984e
Tanisha Purnell <tanishad93@gmail.com>
2017-12-06 10:30:53 -0700

    Updated sign-up and sign-in services

--- src/app/services/sign.up.service.ts
+++ src/app/services/sign.up.service.ts
@@ -3,6 +3,7 @@ import {HttpClient} from "@angular/common/http";
 import {SignUp} from "../classes/Sign.up";
 import {Observable} from "rxjs/Observable";
 import {Status} from "../classes/status";
+import {Profile} from "../classes/profile";
 
 @Injectable()
 export class SignUpService {
@@ -11,6 +12,11 @@ export class SignUpService {
 	}
 
 	private signUpUrl = "api/sign-up/";
+
+	// call to the API and get a Category by Category Name
+	getProfileByProfileEmail (ProfileEmail: string) :Observable<Profile> {
+		return(this.http.get<Profile>(this.signUpUrl + "?ProfileEmail=" + ProfileEmail));
+	}
 
 	createProfile(signUp: SignUp) : Observable<Status> {
 		return(this.http.post<Status>(this.signUpUrl, signUp));
