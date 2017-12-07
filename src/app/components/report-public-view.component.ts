6960374249051daaca8b0fd527bf15837099ed15
Kevin Atkins <kaitoxas@gmail.com>
2017-12-07 12:45:16 -0700

    updated 'report-public-view.component.ts'

--- src/app/components/report-public-view.component.ts
+++ src/app/components/report-public-view.component.ts
@@ -2,8 +2,6 @@ import {Component, OnInit} from "@angular/core";
 import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 import {Report} from "../classes/report";
 import {ReportService} from "../services/report.service";
-import {Category} from "../classes/category";
-import {CategoryService} from "../services/category.service";
 import {Image} from "../classes/image";
 import {ImageService} from "../services/image.service";
 import {Status} from "../classes/status";
@@ -16,8 +14,6 @@ import {Status} from "../classes/status";
 export class ReportPublicViewComponent implements OnInit{
 	reportPublicViewForm: FormGroup;
 
-	category: Category = new Category(null, null);
-
 	report: Report = new Report(null, null, null, null, null, null, null);
 
 	image: Image = new Image(null, null, null, null, null);
@@ -25,24 +21,14 @@ export class ReportPublicViewComponent implements OnInit{
 	//declare needed state variables for later use
 	status: Status = null;
 
-	constructor(private formBuilder: FormBuilder, private reportService: ReportService, private categoryService: CategoryService, private imageService: ImageService){}
+	constructor(private formBuilder: FormBuilder, private reportService: ReportService, private imageService: ImageService){}
 
 	ngOnInit(): void {
-
 		this.getReportByReportId();
-
-		this.reportPublicViewForm = this.formBuilder.group({
-			reportStatus: ["", [Validators.required]],
-			reportUrgency: ["", [Validators.required]],
-			commentContent: ["", [Validators.maxLength(500), Validators.required]]
-		});
-
 	}
 
 	getReportByReportId(): void {
 		this.reportService.getReportByReportId(this.report.reportId)
 			.subscribe(report => this.report = report);
 	}
-
-	// I need to get Image by imageReportId
 }
