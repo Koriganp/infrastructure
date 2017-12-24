1debf0ea0b3903b47de8c03c6886421098f7f52e
Kevin Atkins <kaitoxas@gmail.com>
2017-12-24 14:00:30 -0800

    removed 'Image' class

--- public_html/api/report/index.php
+++ public_html/api/report/index.php
@@ -14,7 +14,7 @@ require_once(dirname(__DIR__, 3) . "/php/lib/jwt.php");
 require_once("/etc/apache2/capstone-mysql/encrypted-config.php");
 
 use Edu\Cnm\Infrastructure\ {
-	Report, Image
+	Report
 };
 
 //verify the xsrf challenge
