abb3559526e39df142020f8259696d608bc6842c
Kevin Atkins <kaitoxas@gmail.com>
2018-01-16 18:52:46 -0800

    minor change

--- src/app/services/report.service.ts
+++ src/app/services/report.service.ts
@@ -58,7 +58,7 @@ export class ReportService {
 	}
 
 	// call to the API and get an array of reports based off the categoryId
-    getReportByReportCategoryId(reportCategoryId : string) : Observable<Report[]> {
+	getReportByReportCategoryId(reportCategoryId : string) : Observable<Report[]> {
 		return(this.http.get<Report[]>(this.reportUrl + "?reportCategoryId="+ reportCategoryId));
 	}
 
