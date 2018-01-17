6199f7ee967b15a010051562a16e21c0543e0617
Kevin Atkins <kaitoxas@gmail.com>
2018-01-16 18:55:22 -0800

    resolved issue I was having when I would refresh the 'home-view' component (for some reason, it would sat the url could not be found)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -71,8 +71,7 @@ export const routes: Routes = [
 	{path: "foot", component: FootComponent},
 	{path: "category", component: CategoryComponent},
 	{path: "infrastructure-app", component: InfrastructureAppComponent},
-	{path: "home-view", component: HomeViewComponent},
-	{ path: '', redirectTo: '/home-view', pathMatch: 'full'}
+	{path: "", component: HomeViewComponent}
 ];
 
 // an array of services that will be passed off to the module
